122d4e1ed286b281c4e267a54ad8c40540d4682f
raneuronuae <neuronpd@gmail.com>
2026-07-13 00:37:29 +0400

    feat: add auto-superuser creation

--- core_assets/apps.py
+++ core_assets/apps.py
@@ -6,3 +6,8 @@ class CoreAssetsConfig(AppConfig):
 
     def ready(self):
         import core_assets.signals  # এই লাইনটি আপনার signals.py কে সক্রিয় করবে
+
+    # নতুন সুপারইউজার তৈরির কোড
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'password123')
